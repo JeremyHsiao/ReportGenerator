e56dbe8c26bc85dc3bc9b2f7e14eac321a9b3251
Jeremy Hsiao <jeremy.hsiao@auo.com>
2022-08-15 10:35:12 +0800

    Base code.
    1st version under git control.

---
+++ ExcelApp/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_run = new System.Windows.Forms.Button();
+            this.MsgWindow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_run
+            // 
+            this.btn_run.Location = new System.Drawing.Point(12, 226);
+            this.btn_run.Name = "btn_run";
+            this.btn_run.Size = new System.Drawing.Size(75, 23);
+            this.btn_run.TabIndex = 0;
+            this.btn_run.Text = "Run";
+            this.btn_run.UseVisualStyleBackColor = true;
+            this.btn_run.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MsgWindow
+            // 
+            this.MsgWindow.Location = new System.Drawing.Point(12, 12);
+            this.MsgWindow.Multiline = true;
+            this.MsgWindow.Name = "MsgWindow";
+            this.MsgWindow.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MsgWindow.Size = new System.Drawing.Size(490, 208);
+            this.MsgWindow.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 261);
+            this.Controls.Add(this.MsgWindow);
+            this.Controls.Add(this.btn_run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_run;
+        private System.Windows.Forms.TextBox MsgWindow;
+    }
+}
+
