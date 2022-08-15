9f3a6c0da652b77be856acb91a52ddbd9be7d82a
Jeremy Hsiao <jeremy.hsiao@auo.com>
2022-08-15 16:35:39 +0800

    Create class TestCase and Create a TC-list generation function -- To be tested.

--- ExcelApp/ReportWorker.cs
+++ ExcelApp/ReportWorker.cs
@@ -140,6 +140,60 @@ Returns or sets the type of underline applied to the font.
         }
     }
 
+    public class TestCase
+    {
+        private String key;
+        private String group;
+        private String summary;
+        private String status;
+        private String links;
+
+        public String Key   // property
+        {
+            get { return key; }   // get method
+            set { key = value; }  // set method
+        }
+
+        public String Group   // property
+        {
+            get { return group; }   // get method
+            set { group = value; }  // set method
+        }
+
+        public String Summary   // property
+        {
+            get { return Summary; }   // get method
+            set { Summary = value; }  // set method
+        }
+
+        public String Status   // property
+        {
+            get { return Status; }   // get method
+            set { Status = value; }  // set method
+        }
+
+        public String Links   // property
+        {
+            get { return links; }   // get method
+            set { links = value; }  // set method
+        }
+
+        public const string col_Key = "Key";
+        public const string col_Group = "Test Group";
+        public const string col_Summary = "Summary";
+        public const string col_Status = "Status";
+        public const string col_Links = "Links";
+
+        public TestCase()
+        {
+        }
+
+        public TestCase(String key, String group, String summary, String status, String links)
+        {
+            this.key = key; this.group = group; this.summary = summary; this.status = status; this.links = links;
+        }
+    }
+
     static class ReportWorker
     {
 
@@ -151,6 +205,8 @@ Returns or sets the type of underline applied to the font.
         // Key value
         const string BUG_KEY = "BENSE";
         const string TC_KEY = "TCBEN";
+
+        static public List<TestCase> testcase_list = new List<TestCase> ();
 
         static public Dictionary<string, int> CreateTableColumnIndex(Worksheet bug_worksheet, int naming_row)
         {
@@ -332,6 +388,47 @@ Returns or sets the type of underline applied to the font.
             }
         }
 
+        static public List<TestCase> GenerateTestCaseList(string tclist_filename)
+        {
+            List<TestCase> ret_tc_list = new List<TestCase>();
+
+            // Open excel (read-only & corrupt-load)
+            Excel.Application myTCExcel = ExcelAction.OpenPreviousExcel(tclist_filename);
+            //Excel.Application myTCExcel = OpenOridnaryExcel(tclist_filename);
+            if (myTCExcel != null)
+            {
+                Worksheet WorkingSheet = ExcelAction.Find_Worksheet(myTCExcel, sheet_TC_Jira);
+                if (WorkingSheet != null)
+                {
+                    const int row_column_naming = 1;
+                    Dictionary<string, int> col_name_list = CreateTableColumnIndex(WorkingSheet, row_column_naming);
+
+                    // Get the last (row,col) of excel
+                    Range rngLast = WorkingSheet.get_Range("A1").SpecialCells(Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeLastCell);
+
+                    // Visit all rows and replace Bug-ID with long description of Bug.
+                    const int row_tc_starting = 2;
+                    for (int index = row_tc_starting; index <= rngLast.Row; index++)
+                    {
+                        Object cell_value2;
+
+                        // Make sure Key of TC is not empty
+                        cell_value2 = WorkingSheet.Cells[index, col_name_list[TestCase.col_Key]].Value2;
+                        if (cell_value2 == null) { break; }
+                        String key = cell_value2.ToString(),
+                                        group = WorkingSheet.Cells[index, col_name_list[TestCase.col_Group]].Value2.ToString(),
+                                        summary = WorkingSheet.Cells[index, col_name_list[TestCase.col_Summary]].Value2.ToString(),
+                                        status = WorkingSheet.Cells[index, col_name_list[TestCase.col_Group]].Value2.ToString(),
+                                        links = WorkingSheet.Cells[index, col_name_list[TestCase.col_Summary]].Value2.ToString();
+                        ret_tc_list.Add(new TestCase(key, group, summary, status, links));
+                    }
+                }
+                ExcelAction.CloseExcelWithoutSaveChanges(myTCExcel);
+                myTCExcel = null;
+            }
+            return ret_tc_list;
+        }
+
         static public void ProcessTCJiraExcel(string tclist_filename, Dictionary<string, List<StyleString>> bug_list)
         {
             // Open excel (read-only & corrupt-load)
@@ -349,20 +446,18 @@ Returns or sets the type of underline applied to the font.
                     Range rngLast = WorkingSheet.get_Range("A1").SpecialCells(Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeLastCell);
 
                     // Visit all rows and replace Bug-ID with long description of Bug.
-                    const string col_Key = "Key";
-                    const string col_Links = "Links";
                     const int row_tc_starting = 2;
                     for (int index = row_tc_starting; index <= rngLast.Row; index++)
                     {
                         Object cell_value2;
 
                         // Make sure Key of TC is not empty
-                        cell_value2 = WorkingSheet.Cells[index, col_name_list[col_Key]].Value2;
+                        cell_value2 = WorkingSheet.Cells[index, col_name_list[TestCase.col_Key]].Value2;
                         if (cell_value2 == null) { break; }
                         if (cell_value2.ToString().Contains(TC_KEY) == false) { break; }
 
                         // If Links is not empty, extend bug key into long string with font settings
-                        Range rng = WorkingSheet.Cells[index, col_name_list[col_Links]];
+                        Range rng = WorkingSheet.Cells[index, col_name_list[TestCase.col_Links]];
                         cell_value2 = rng.Value2;
                         if (cell_value2 != null)
                         {
