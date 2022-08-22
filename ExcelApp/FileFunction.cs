1659811ef5f4e7310103fa0025b35786b8d584ec
Jeremy Hsiao <jeremy.hsiao@auo.com>
2022-08-22 10:32:40 +0800

    1. Create separate file for File-operation related function. 2. Revise default filename handling (use only verbatim indentifier at default filename so that no need to use it later & also getting full-path during form load)

---
+++ ExcelApp/FileFunction.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace ExcelReportApplication
+{
+    class FileFunction
+    {
+        static public String UsesrSelectFilename()
+        {
+            String ret_str = "";
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Title = "Select file";
+            dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            dialog.Filter = "Excel files (*.xls/xlsx)|*.xls;*.xlsx";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ret_str = dialog.FileName;
+                // It seems that File-existing check is done after "Select" button is pressed
+                // So no need to check here.
+                /*
+                if (!CheckFileExist(ret_str))
+                {
+                    MessageBox.Show("Selected file does not exist so filename remains unchanged.\n");
+                    ret_str = "";
+                }
+                */
+            }
+            return ret_str;
+        }
+
+        static public bool CheckFileExist(String Filename)
+        {
+            bool ret;
+            if (!File.Exists(Filename))
+            {
+                ret = false;
+            }
+            else
+            {
+                ret = true;
+            }
+            return ret;
+        }
+
+        static public String GetFullFilePath(String Filename)
+        {
+            String ret ="";
+            try
+            {
+                ret = Path.GetFullPath(Filename);
+            }
+            catch
+            {
+                // "" will be returned if exceptions
+            }
+            return ret;
+        }
+    }
+}
--- ExcelApp/MainForm.cs
+++ ExcelApp/MainForm.cs
@@ -6,10 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using System.IO;
-using System.Globalization;
-using Microsoft.Office.Interop.Excel;
-using Excel = Microsoft.Office.Interop.Excel; 
 
 namespace ExcelReportApplication
 {
@@ -22,20 +18,23 @@ namespace ExcelReportApplication
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            String default_data_path = @".\SampleData\";
-            buglist_filename = default_data_path + workbook_BUG_Jira;
-            tclist_filename = default_data_path + workbook_TC_Jira;
-            report_filename = default_data_path + workbook_Report;
-            txtBugFile.Text = buglist_filename;
-            txtTCFile.Text = tclist_filename;
-            txtReportFile.Text = report_filename;
+            txtBugFile.Text = workbook_BUG_Jira;
+            txtTCFile.Text = workbook_TC_Jira;
+            txtReportFile.Text = workbook_Report;
+            buglist_filename = FileFunction.GetFullFilePath(workbook_BUG_Jira);
+            tclist_filename = FileFunction.GetFullFilePath(workbook_TC_Jira);
+            report_filename = FileFunction.GetFullFilePath(workbook_Report);
+            if ((buglist_filename == "") || (tclist_filename == "") || (report_filename == ""))
+            {
+                MsgWindow.AppendText("WARNING: some sample data do not exist.\n");
+            }
         }
 
         // constant strings for workbook used in this application.
         //const string workbook_test_buglist = "SE27205_0803.xlsx";
-        const string workbook_BUG_Jira = "Jira 2022-08-12T15_20_08+0800.xls";
-        const string workbook_TC_Jira = "TC_Jira 2022-08-12T15_16_38+0800.xls";
-        const string workbook_Report = "Report_Template.xlsx";
+        const string workbook_BUG_Jira = @".\SampleData\Jira 2022-08-12T15_20_08+0800.xls";
+        const string workbook_TC_Jira = @".\SampleData\TC_Jira 2022-08-12T15_16_38+0800.xls";
+        const string workbook_Report = @".\SampleData\Report_Template.xlsx";
 
         private string buglist_filename, tclist_filename, report_filename, args_str;
 
@@ -43,76 +42,62 @@ namespace ExcelReportApplication
         {
             // Create global bug list
             // BUG_Jira
-            args_str = Path.GetFullPath(buglist_filename);
-            if (!File.Exists(@args_str))
+            if (!FileFunction.CheckFileExist(buglist_filename))
             {
-                MsgWindow.AppendText(args_str + " does not exist. Please check again.\n");
+                MsgWindow.AppendText(buglist_filename + " does not exist. Please check again.\n");
                 return;
             }
             else
             {
-                MsgWindow.AppendText("Processing bug_list:" + args_str + ".\n");
-                ReportWorker.global_bug_list = ReportWorker.ProcessBugList(@args_str);
+                MsgWindow.AppendText("Processing bug_list:" + buglist_filename + ".\n");
+                ReportWorker.global_bug_list = ReportWorker.ProcessBugList(buglist_filename);
                 MsgWindow.AppendText("bug_list finished!\n");
             }
 
             // Create global TestCase list
-            args_str = Path.GetFullPath(tclist_filename);
-            if (!File.Exists(@args_str))
+            if (!FileFunction.CheckFileExist(tclist_filename))
             {
-                MsgWindow.AppendText(args_str + " does not exist. Please check again.\n");
+                MsgWindow.AppendText(tclist_filename + " does not exist. Please check again.\n");
                 return;
             }
             else
             {
-                MsgWindow.AppendText("Processing tc_list:" + args_str + ".\n");
-                ReportWorker.global_testcase_list = ReportWorker.GenerateTestCaseList(@args_str);
+                MsgWindow.AppendText("Processing tc_list:" + tclist_filename + ".\n");
+                ReportWorker.global_testcase_list = ReportWorker.GenerateTestCaseList(tclist_filename);
                 MsgWindow.AppendText("tc_list finished!\n");
             }
 
-            /*
+            ///*
             // Write extended string back to tc-file
-            tclist_filename = workbook_TC_Jira;
-            args_str = Path.GetFullPath(tclist_filename);
-            if (File.Exists(@args_str))
+            if (FileFunction.CheckFileExist(tclist_filename))
             {
-                ReportWorker.WriteBacktoTCJiraExcel(@args_str);
+                ReportWorker.WriteBacktoTCJiraExcel(tclist_filename);
+                MsgWindow.AppendText("Writeback sample to tc_list finished!\n");
             }
-            */
+            //*/
 
             // Write extended string to report-file (fill template and save as other file)
-            args_str = Path.GetFullPath(report_filename);
-            if (!File.Exists(@args_str))
+            if (!FileFunction.CheckFileExist(report_filename))
             {
                 MsgWindow.AppendText("Report file template does not exist. Please check again.\n");
                 return;
             }
             else
             {
-                ReportWorker.SaveToReportTemplate(@args_str);
+                ReportWorker.SaveToReportTemplate(report_filename);
                 MsgWindow.AppendText("report finished!\n");
             }
 
             GC.Collect();
         }
 
-        private String AskForFilename()
-        {
-            String ret_str = "";
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Title = "Select file";
-            dialog.InitialDirectory = Directory.GetCurrentDirectory();
-            dialog.Filter = "Excel files (*.xls/xlsx)|*.xls;*.xlsx";
-            if (dialog.ShowDialog() == DialogResult.OK)
-            {
-                ret_str = dialog.FileName;
-            }
-            return ret_str;
-        }
-
+        // Because TextBox is set to Read-only, filename can be only changed via File Dialog
+        // (1) no need to handle event of TestBox Text changed.
+        // (2) filename (full path) is set only after File Dialog OK
+        // (3) no user input --> no relative filepath --> no need to update fileanem from relative path to full path.
         private void btnSelectBugFile_Click(object sender, EventArgs e)
         {
-            String ret_str = AskForFilename();
+            String ret_str = FileFunction.UsesrSelectFilename();
             if (ret_str != "")
             {
                 buglist_filename = ret_str;
@@ -122,7 +107,7 @@ namespace ExcelReportApplication
 
         private void btnSelectTCFile_Click(object sender, EventArgs e)
         {
-            String ret_str = AskForFilename();
+            String ret_str = FileFunction.UsesrSelectFilename();
             if (ret_str != "")
             {
                 tclist_filename = ret_str;
@@ -132,7 +117,7 @@ namespace ExcelReportApplication
 
         private void btnSelectReportFile_Click(object sender, EventArgs e)
         {
-            String ret_str = AskForFilename();
+            String ret_str = FileFunction.UsesrSelectFilename();
             if (ret_str != "")
             {
                 report_filename = ret_str;
