a5a40256aa9eb7f11293ed85389c268601f1d9ca
Jeremy Hsiao <jeremy.hsiao@auo.com>
2022-08-22 14:52:06 +0800

    Update settings

--- ExcelApp/Properties/AssemblyInfo.cs
+++ ExcelApp/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 組件的一般資訊是由下列的屬性集控制。
 // 變更這些屬性的值即可修改組件的相關
 // 資訊。
-[assembly: AssemblyTitle("ExcelApp")]
+[assembly: AssemblyTitle("ReportGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ExcelApp")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("ReportGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2022 AUO Corporation")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- ExcelApp/ReportWorker.cs
+++ ExcelApp/ReportWorker.cs
@@ -153,59 +153,6 @@ Returns or sets the type of underline applied to the font.
         }
     }
 
-    public class TestCase
-    {
-        private String key;
-        private String group;
-        private String summary;
-        private String status;
-        private String links;
-
-        public String Key   // property
-        {
-            get { return key; }   // get method
-            set { key = value; }  // set method
-        }
-
-        public String Group   // property
-        {
-            get { return group; }   // get method
-            set { group = value; }  // set method
-        }
-
-        public String Summary   // property
-        {
-            get { return summary; }   // get method
-            set { summary = value; }  // set method
-        }
-
-        public String Status   // property
-        {
-            get { return status; }   // get method
-            set { status = value; }  // set method
-        }
-
-        public String Links   // property
-        {
-            get { return links; }   // get method
-            set { links = value; }  // set method
-        }
-
-        public const string col_Key = "Key";
-        public const string col_Group = "Test Group";
-        public const string col_Summary = "Summary";
-        public const string col_Status = "Status";
-        public const string col_Links = "Linked Issues";
-        public TestCase()
-        {
-        }
-
-        public TestCase(String key, String group, String summary, String status, String links)
-        {
-            this.key = key; this.group = group; this.summary = summary; this.status = status; this.links = links;
-        }
-    }
-
     static class ReportWorker
     {
 
