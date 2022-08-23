3b9bacdfceb459100cc3da76764d74e3e304396f
Jeremy Hsiao <jeremy.hsiao@auo.com>
2022-08-23 17:27:16 +0800

    (1) Add TestPlan.cs for class TestPlan (2) Generate a base TestReport File Structure

--- ExcelApp/ExcelTest.cs
+++ ExcelApp/ExcelTest.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
 
 namespace ExcelReportApplication
 {
@@ -14,21 +19,64 @@ namespace ExcelReportApplication
             {
                 return false;
             }
+            GenerateTestReportStructure(test_filename);
             return true;
         }
 
-        public static void ReadSheet_TestPlan()
+        static List<TestPlan> global_tp = new List<TestPlan>();
+        static public string SheetName_TestPlan = "Test Plan";
+        public static void GenerateTestReportStructure(String report_filename)
         {
+            List<TestPlan> ret_testplan = new List<TestPlan>();
+
+            // open standard test report
+            Excel.Application myReportExcel = ExcelAction.OpenOridnaryExcel(report_filename);
+            if (myReportExcel == null)
+            {
+                Console.WriteLine("OpenOridnaryExcel failed in GenerateTestReportStructure()");
+                return;
+            }
+
+            // Select and read Test Plan sheet
+            Worksheet testplan_ws = ExcelAction.Find_Worksheet(myReportExcel, SheetName_TestPlan);
+            if (testplan_ws == null)
+            {
+                Console.WriteLine("Find_Worksheet (TestPlan) failed in GenerateTestReportStructure()");
+                return;
+            }
+            global_tp = TestPlan.LoadTestPlanSheet(testplan_ws);
+
+            // Create a list of folder to be created and files to be copied (from/to)
+            // filtered by Do or Not
+            List<String> folder = new List<String>(), from = new List<String>(), to = new List<String>();
+            foreach (TestPlan tp in global_tp)
+            {
+                String group = tp.Group, summary = tp.Summary, do_or_not = tp.DoOrNot, subpart = tp.Subpart;
+                if (do_or_not == "V")
+                {
+                    if (!folder.Contains(group)) { folder.Add(group); }
+                    from.Add(@"Database Backup\" + summary.Substring(0, summary.IndexOf('_')) + ".xlsx");
+                    to.Add(group + @"\" + summary + "_" + subpart + ".xlsx");
+                }
+            }
+
+            // Create folder
+            String input_report_dir = Path.GetDirectoryName(report_filename);
+            String output_report_dir = input_report_dir + @"\TestReport_" + DateTime.Now.ToString("yyyyMMddHHmmss");
+            Directory.CreateDirectory(output_report_dir);
+            foreach (String folder_name in folder)
+            {
+                Directory.CreateDirectory(output_report_dir + @"\" + folder_name);
+            }
+
+            // Copy files
+            for (int index = 0; index < from.Count; index++)
+            {
+                String src = input_report_dir + @"\" + from[index];
+                String dest = output_report_dir + @"\" + to[index];
+                File.Copy(src, dest);
+            }
         }
 
-        public static void GenerateTestReportStructure()
-        {
-            // open standard test report
-            // read Test Plan sheet
-            // filtered by Do or Not
-            // chekck folder and create it if not exist
-            // check excel and copy it if not exist
-            // option: remove "Not" folder/file
-        }
     }
 }
---
+++ ExcelApp/TestPlan.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelReportApplication
+{
+    public class TestPlan
+    {
+        private String group;
+        private String summary;
+        private String assignee;
+        private String do_or_not;
+        private String category;
+        private String subpart;
+
+        public String Group   // property
+        {
+            get { return group; }   // get method
+            set { group = value; }  // set method
+        }
+
+        public String Summary   // property
+        {
+            get { return summary; }   // get method
+            set { summary = value; }  // set method
+        }
+
+        public String Assignee   // property
+        {
+            get { return assignee; }   // get method
+            set { assignee = value; }  // set method
+        }
+
+        public String DoOrNot   // property
+        {
+            get { return do_or_not; }   // get method
+            set { do_or_not = value; }  // set method
+        }
+
+        public String Category   // property
+        {
+            get { return category; }   // get method
+            set { category = value; }  // set method
+        }
+
+        public String Subpart   // property
+        {
+            get { return subpart; }   // get method
+            set { subpart = value; }  // set method
+        }
+
+        public TestPlan()
+        {
+        }
+
+        public TestPlan(List<String> members)
+        {
+            this.group = members[(int)TestPlanMemberIndex.GROUP];
+            this.summary = members[(int)TestPlanMemberIndex.SUMMARY];
+            this.assignee = members[(int)TestPlanMemberIndex.ASSIGNEE];
+            this.do_or_not = members[(int)TestPlanMemberIndex.DO_OR_NOT];
+            this.category = members[(int)TestPlanMemberIndex.CATEGORY];
+            this.subpart = members[(int)TestPlanMemberIndex.SUBPART];
+        }
+
+        enum TestPlanMemberIndex
+        {
+            GROUP = 0,
+            SUMMARY,
+            ASSIGNEE,
+            DO_OR_NOT,
+            CATEGORY,
+            SUBPART,
+            MAX_NO
+        }
+        // The sequence of this String[] must be aligned with enum TestPlanMemberIndex
+        static String[] TestPlanMemberColumnName = { "Test Group", "Summary", "Assginee", "Do or Not", "Test Case Category", "Subpart" };
+
+        static public int NameDefinitionRow_TestPlan = 2;
+        static public int DataBeginRow_TestPlan = 3;
+
+        public static List<TestPlan> LoadTestPlanSheet(Worksheet testplan_ws)
+        {
+            List<TestPlan> ret_testplan = new List<TestPlan>();
+
+            // Create index for each column name
+            Dictionary<string, int> col_name_list = ExcelAction.CreateTableColumnIndex(testplan_ws, NameDefinitionRow_TestPlan);
+
+            // Get the last (row,col) of excel
+            Range rngLast = testplan_ws.get_Range("A1").SpecialCells(Microsoft.Office.Interop.Excel.XlCellType.xlCellTypeLastCell);
+
+            // Visit all rows and add content 
+            for (int index = DataBeginRow_TestPlan; index <= rngLast.Row; index++)
+            {
+                List<String> members = new List<String>();
+                for (int member_index = 0; member_index < (int)TestPlanMemberIndex.MAX_NO; member_index++)
+                {
+                    Object cell_value2 = testplan_ws.Cells[index, col_name_list[TestPlanMemberColumnName[member_index]]].Value2;
+                    String str = (cell_value2 == null) ? "" : cell_value2.ToString();
+                    if (str == "")
+                    {
+                        break; // cannot be empty value; skip to next row
+                    }
+                    members.Add(str);
+                }
+                if (members.Count == (int)TestPlanMemberIndex.MAX_NO)
+                {
+                    TestPlan tp = new TestPlan(members);
+                    ret_testplan.Add(tp);
+                }
+            }
+            return ret_testplan;
+        }
+    }
+
+
+}
