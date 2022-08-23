ee1ecbfa45267486614388d96014a3fb9c693d89
Jeremy Hsiao <jeremy.hsiao@auo.com>
2022-08-23 13:59:07 +0800

    Separate class StyleString

---
+++ ExcelApp/StyleString.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Globalization;
+
+namespace ExcelReportApplication
+{
+    /*  Font property
+* 
+Background	
+Returns or sets the type of background for text used in charts. Can be one of the XlBackground constants.
+
+Bold	
+True if the font is bold.
+
+Color	
+Returns or sets the primary color of the font.
+
+ColorIndex	
+Returns or sets the color of the font.
+
+Creator	
+Returns a 32-bit integer that indicates the application in which this object was created.
+
+FontStyle	
+Returns or sets the font style.
+
+Italic	
+True if the font style is italic.
+
+OutlineFont	
+True if the font is an outline font.
+
+Shadow	
+True if the font is a shadow font or if the object has a shadow.
+
+Size	
+Returns or sets the size of the font.
+
+Strikethrough	
+True if the font is struck through with a horizontal line.
+
+Subscript	
+True if the font is formatted as subscript. False by default.
+
+Superscript	
+True if the font is formatted as superscript. False by default.
+
+ThemeColor	
+Returns or sets the theme color in the applied color scheme that is associated with the specified object. Read/write Object.
+
+ThemeFont	
+Returns or sets the theme font in the applied font scheme that is associated with the specified object. Read/write XlThemeFont.
+
+TintAndShade	
+Returns or sets a Single that lightens or darkens a color.
+v
+Underline	
+Returns or sets the type of underline applied to the font.
+*/
+
+    public class StyleString
+    {
+        private String text;
+        private String font_name;
+        private Color font_color;
+        private int font_size;
+        private FontStyle font_style;
+        private bool font_property_changed;
+
+        public String Text   // property
+        {
+            get { return text; }   // get method
+            set { text = value; }  // set method
+        }
+        public String Font   // property
+        {
+            get { return font_name; }   // get method
+            set { font_name = value; font_property_changed = true; }  // set method
+        }
+        public Color Color   // property
+        {
+            get { return font_color; }   // get method
+            set { font_color = value; font_property_changed = true; }  // set method
+        }
+        public int Size   // property
+        {
+            get { return font_size; }   // get method
+            set { font_size = value; font_property_changed = true; }  // set method
+        }
+        public FontStyle FontStyle   // property
+        {
+            get { return font_style; }   // get method
+            set { font_style = value; font_property_changed = true; }  // set method
+        }
+        public bool FontPropertyChanged  // property
+        {
+            get { return font_property_changed; }   // get method
+        }
+
+        static public string default_font = "Gill Sans MT";
+        static public int default_size = 10;
+        static public Color default_color = System.Drawing.Color.Black;
+        static public FontStyle default_fontstyle = FontStyle.Regular;
+
+        public void SetProperty(Color string_color, string string_fontname, int string_fontsize, FontStyle string_fontstyle)
+        {
+            font_color = string_color;
+            font_name = string_fontname;
+            font_size = string_fontsize;
+            font_style = string_fontstyle;
+            font_property_changed = true;
+        }
+
+        public void SetDefaultProperty()
+        {
+            SetProperty(default_color, default_font, default_size, default_fontstyle);
+            font_property_changed = false;
+        }
+
+        public void SetDefaultProperty(String string_text)
+        {
+            SetDefaultProperty();
+            Text = string_text;
+        }
+
+        public StyleString()
+        {
+            SetDefaultProperty("");
+        }
+
+        public StyleString(string string_text)
+        {
+            SetDefaultProperty(string_text);
+        }
+
+        public StyleString(string string_text, Color string_color)
+        {
+            SetDefaultProperty(string_text);
+            Color = string_color;
+            text = string_text;
+        }
+
+        public StyleString(string string_text, Color string_color, string string_fontname, int string_fontsize)
+        {
+            SetProperty(string_color, string_fontname, string_fontsize, default_fontstyle);
+            text = string_text;
+        }
+    }
+
+}
