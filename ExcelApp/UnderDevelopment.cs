08a8aa1d555cc52e974a16d8de68b56649ca5541
Jeremy Hsiao <jeremy.hsiao@outlook.com>
2022-09-20 14:23:39 +0800

    Add Keyword.cs and relocate function calls into it.

--- ExcelApp/UnderDevelopment.cs
+++ ExcelApp/UnderDevelopment.cs
@@ -9,24 +9,4 @@ namespace ExcelReportApplication
     static class UnderDevelopment
     {
     }
-
-    public static class KeywordReport
-    {
-        static public List<TestPlanKeyword> ListAllKeyword(List<TestPlan> DoPlan)
-        {
-            List<TestPlanKeyword> ret = new List<TestPlanKeyword>();
-            foreach (TestPlan plan in DoPlan)
-            {
-                plan.OpenDetailExcel();
-                List<TestPlanKeyword> plan_keyword = plan.ListKeyword();
-                plan.CloseIssueListExcel();
-                if (plan_keyword!=null)
-                {
-                    ret.AddRange(plan_keyword);
-                }
-            }
-            return ret;
-        }
-    }
-
 }
