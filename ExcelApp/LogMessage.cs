8c8d5941037957d4e55f7a08fa3e3668348f3003
Jeremy Hsiao <jeremy.hsiao@outlook.com>
2023-12-28 14:47:53 +0800

    Store internal-use console message into SYSTEM_LOG

--- ExcelApp/LogMessage.cs
+++ ExcelApp/LogMessage.cs
@@ -9,22 +9,32 @@ namespace ExcelReportApplication
     {
         static public void CheckFunctionAt(String function, int row)
         {
-            Console.WriteLine("Warning: please check " + function + " at " + row.ToString());
+            String message_line;
+            message_line = "Warning: please check " + function + " at " + row.ToString();
+            Console.WriteLine(message_line);
+            MainForm.SystemLogAddLine(message_line);
         }
 
         static public void CheckFunction(String function)
         {
-            Console.WriteLine("Warning: please check " + function);
+            String message_line;
+            message_line = "Warning: please check " + function;
+            Console.WriteLine(message_line);
+            MainForm.SystemLogAddLine(message_line);
         }
 
         static public void CheckFunctionAtRowColumn(String function, int row, int col)
         {
-            Console.WriteLine("Warning: please check " + function + " at (" + row.ToString() + "," + col.ToString() + ")");
+            String message_line;
+            message_line = "Warning: please check " + function + " at (" + row.ToString() + "," + col.ToString() + ")";
+            Console.WriteLine(message_line);
+            MainForm.SystemLogAddLine(message_line);
         }
 
         static public void WriteLine(String message)
         {
             Console.WriteLine(message);
+            MainForm.SystemLogAddLine(message);
         }
     }
 }
